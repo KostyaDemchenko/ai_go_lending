969850cc14a5b0e01d8a35c766f1bd91ea29a812
Jpeg <kostya.doge@gmail.com>
2025-02-08 00:58:08 +0200

    --proj init

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+// import HeadComponent from "@/src/components/Head";
+// import Header from "@/src/components/Header";
+// import Footer from "@/src/components/Footer";
+
+// import styles from "@/src/app/global.scss";
+
+export default function Home() {
+  return (
+    <div className='hero'>
+      {/* <HeadComponent />
+      <section className='main-container'>
+        <Header />
+        <main className='hero-main'></main>
+        <Footer />
+      </section> */}
+    </div>
+  );
+}
