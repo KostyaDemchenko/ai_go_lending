914ed93348312c5c420252babf10e1eb2434c40b
KostyaDemchenko <kostya.doge@gmail.com>
2025-02-09 17:23:29 +0200

    --added: Added Head to amin page for testing analitics collections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-// import HeadComponent from "@/src/components/Head";
+import HeadComponent from "@/src/components/Head";
 // import Header from "@/src/components/Header";
 // import Footer from "@/src/components/Footer";
 
@@ -7,8 +7,11 @@
 export default function Home() {
   return (
     <div className='hero'>
-      {/* <HeadComponent />
-      <section className='main-container'>
+      <HeadComponent
+        pageTitle='AiGo — перша українська бібліотека нейромереж'
+        pageDescription='AiGo — перша в Україні бібліотека нейромереж, що допомагає швидко знайти, порівняти й обрати найкращі AI-рішення для бізнесу чи творчості. Приєднуйтеся, щоб відкрити можливості штучного інтелекту вже сьогодні!'
+      />
+      {/* <section className='main-container'>
         <Header />
         <main className='hero-main'></main>
         <Footer />
